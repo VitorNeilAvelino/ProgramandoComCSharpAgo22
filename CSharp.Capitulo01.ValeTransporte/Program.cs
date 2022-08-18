ff0580f966d32c6b91343425a4caec402003b947
Vitor Neil Avelino <avelino.vitor@gmail.com>
2022-08-17 23:07:30 -0300

    Começo do projeto de Vale Transporte

---
+++ CSharp.Capitulo01.ValeTransporte/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp.Capitulo01.ValeTransporte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
