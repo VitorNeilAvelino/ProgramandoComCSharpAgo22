fccb74d56798104ec9a46f1ac069901d79432288
Vitor Neil Avelino <avelino.vitor@gmail.com>
2022-08-18 20:23:19 -0300

    Vale Transporte

--- CSharp.Capitulo01.ValeTransporte/Program.cs
+++ CSharp.Capitulo01.ValeTransporte/Program.cs
@@ -6,7 +6,39 @@ namespace CSharp.Capitulo01.ValeTransporte
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Inicio:
+            
+            Console.Write("Funcionário: ");
+            var nome = Console.ReadLine();
+
+            Console.Write("Salário: ");
+            var salario = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Gasto com transporte: ");
+            var gastoComTransporte = Convert.ToDecimal(Console.ReadLine());
+
+            var descontoMaximo = salario * 6 / 100;
+
+            var descontoVT = gastoComTransporte > descontoMaximo ? descontoMaximo : gastoComTransporte;
+
+            var resultado = $"\nFuncionário: {nome}\n" +
+                $"Salário: {salario:c}\n" +
+                $"Desconto VT: {descontoVT:C}";
+
+            Console.WriteLine(resultado);
+
+            Console.WriteLine("\nPressione Enter para novo cálculo ou Esc para sair.");
+
+            var comando = Console.ReadKey();
+
+            if (comando.Key == ConsoleKey.Escape)
+            {
+                Environment.Exit(0);
+            }
+
+            Console.Clear();
+
+            goto Inicio;
         }
     }
 }
