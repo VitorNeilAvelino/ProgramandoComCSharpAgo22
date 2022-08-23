3597c18d66f9b5c3bf6b75a37eea72678061a869
Vitor Neil Avelino <avelino.vitor@gmail.com>
2022-08-22 22:50:59 -0300

    Modelagem do Fintech

---
+++ Fintech.Dominio/Entidades/ContaCorrente.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fintech.Dominio.Entidades
+{
+    class ContaCorrente
+    {
+        public int Numero { get; set; }
+        public string DigitoVerificador { get; set; }
+        public decimal Saldo { get; set; }
+        public Agencia Agencia { get; set; }
+        public Cliente Cliente { get; set; }
+
+        public void EfetuarOperacao(decimal valor, Operacao operacao)
+        {
+            switch (operacao)
+            {
+                case Operacao.Deposito:
+                    Saldo += valor;
+                    break;
+                case Operacao.Saque:
+                    Saldo -= valor;
+                    break;
+            }
+        }
+    }
+}
