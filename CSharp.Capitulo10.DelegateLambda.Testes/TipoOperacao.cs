ee0af01dbcc0c55cd635594c7fb4c854d803fa34
Vitor Neil Avelino <avelino.vitor@gmail.com>
2022-08-31 21:27:57 -0300

    Expressão lâmbda

---
+++ CSharp.Capitulo10.DelegateLambda.Testes/TipoOperacao.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharp.Capitulo10.DelegateLambda.Testes
+{
+    public enum TipoOperacao
+    {
+        Soma = 1,
+        Subtracao = 2,
+        Divisao = 3,
+        Multiplicacao = 4
+    }
+}
