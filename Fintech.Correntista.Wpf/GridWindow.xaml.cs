479369fac0d28f2011c0c6a8f4ffe70212dc3398
Vitor Neil Avelino <avelino.vitor@gmail.com>
2022-10-30 21:09:15 -0300

    Marcar células do Grid

---
+++ Fintech.Correntista.Wpf/GridWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Fintech.Correntista.Wpf
+{
+    /// <summary>
+    /// Lógica interna para GridWindow.xaml
+    /// </summary>
+    public partial class GridWindow : Window
+    {
+        public GridWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            var botaoClicado = sender as Button;
+
+            //var linha = (int)botaoClicado.GetValue(Grid.RowProperty);
+            //var coluna = (int)botaoClicado.GetValue(Grid.ColumnProperty);
+
+            //MessageBox.Show(string.Format("Button clicked at column {0}, row {1}", coluna, linha));
+
+            var todosBotoes = testeGrid.Children
+                                          .Cast<Button>()
+                                          .ToList();
+
+            var botoesSelecionados = testeGrid.Children
+                                              .Cast<Button>()
+                                              .Where(e => Convert.ToInt32(e.Tag) <= Convert.ToInt32(botaoClicado.Tag)).ToList();
+
+            todosBotoes.ForEach(b => b.Background = Brushes.Transparent);
+            botoesSelecionados.ForEach(b => b.Background = Brushes.Red);
+        }
+    }
+}
